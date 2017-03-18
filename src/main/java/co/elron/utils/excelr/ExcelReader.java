0ba0d49f7916a9ce76fec4d4ca73feef64b8ef0b
Ender Aydin Orak <ender@koders.co>
2017-03-18 23:41:17 +0000

    Update supported cell data types, change converter data type to object, add null support & tests

--- src/main/java/co/elron/utils/excelr/ExcelReader.java
+++ src/main/java/co/elron/utils/excelr/ExcelReader.java
@@ -1,158 +1,160 @@
 package co.elron.utils.excelr;
 
+import au.com.bytecode.opencsv.CSVReader;
+import org.apache.poi.POIXMLDocument;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.*;
+
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.poi.POIXMLDocument;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
-
-import au.com.bytecode.opencsv.CSVReader;
-
 public class ExcelReader<T> {
 
-	private static final int MAX_SHEET = 1;
-	private static final char CSV_DELIM = ';';
+    private static final int MAX_SHEET = 1;
+    private static final char CSV_DELIM = ';';
 
-	public static interface RowConverter<T> {
-		public T convert(String[] row);
-	}
+    public interface RowConverter<T> {
+        T convert(Object[] row);
+    }
 
-	public static class Builder<T> {
-		private boolean hasHeader;
-		private RowConverter<T> converter;
-		private int sheets;
-		private char delimiter = CSV_DELIM;
+    public static class Builder<T> {
+        private boolean hasHeader;
+        private RowConverter<T> converter;
+        private int sheets;
+        private char delimiter = CSV_DELIM;
 
-		public Builder() {
-		}
+        public Builder() {
+        }
 
-		public Builder<T> converter(RowConverter<T> converter) {
-			this.converter = converter;
-			return this;
-		}
+        public Builder<T> converter(RowConverter<T> converter) {
+            this.converter = converter;
+            return this;
+        }
 
-		public Builder<T> withHeader() {
-			this.hasHeader = true;
-			return this;
-		}
+        public Builder<T> withHeader() {
+            this.hasHeader = true;
+            return this;
+        }
 
-		public Builder<T> sheets(int sheetCount) {
-			this.sheets = sheetCount;
-			return this;
-		}
+        public Builder<T> sheets(int sheetCount) {
+            this.sheets = sheetCount;
+            return this;
+        }
 
-		public Builder<T> csvDelimiter(char delimiter) {
-			this.delimiter = delimiter;
-			return this;
-		}
+        public Builder<T> csvDelimiter(char delimiter) {
+            this.delimiter = delimiter;
+            return this;
+        }
 
-		public ExcelReader<T> build() {
-			return new ExcelReader<T>(this);
-		}
+        public ExcelReader<T> build() {
+            return new ExcelReader<T>(this);
+        }
 
-	}
+    }
 
-	private Builder<T> info;
+    private Builder<T> info;
 
-	public static <T> Builder<T> builder(Class<T> cls) {
-		return new Builder<T>();
-	}
+    public static <T> Builder<T> builder(Class<T> cls) {
+        return new Builder<T>();
+    }
 
-	private ExcelReader(Builder<T> info) {
-		this.info = info;
-	}
+    private ExcelReader(Builder<T> info) {
+        this.info = info;
+    }
 
-	public List<T> read(String fileName) throws IOException, InvalidFormatException {
-		try (FileInputStream is = new FileInputStream(fileName)) {
-			return read(is);
-		}
-	}
+    public List<T> read(String fileName) throws Exception {
+        try (FileInputStream is = new FileInputStream(fileName)) {
+            return read(is);
+        }
+    }
 
-	public List<T> read(InputStream is) throws IOException, InvalidFormatException {
-		List<T> objList = null;
-		try (BufferedInputStream buf = new BufferedInputStream(is)) {
-			if (isExcel(buf)) { // XLSX, XLS
-				objList = readExcel(buf);
-			} else { // CSV
-				objList = readCsv(buf);
-			}
-		}
-		return objList;
-	}
+    public List<T> read(InputStream is) throws Exception {
+        List<T> objList = null;
+        try (BufferedInputStream buf = new BufferedInputStream(is)) {
+            if (isExcel(buf)) { // XLSX, XLS
+                objList = readExcel(buf);
+            } else { // CSV
+                objList = readCsv(buf);
+            }
+        }
+        return objList;
+    }
 
-	private List<T> readExcel(InputStream is) throws InvalidFormatException, IOException {
-		Workbook workbook = WorkbookFactory.create(is);
-		int sheetCount = Math.min(workbook.getNumberOfSheets(), MAX_SHEET);
-		List<T> objList = new ArrayList<>();
-		sheetCount = (info.sheets == 0) ? sheetCount : info.sheets;
-		for (int i = 0; i < sheetCount; i++) {
-			Sheet sheet = workbook.getSheetAt(i);
-			extractSheet(sheet, objList);
-		}
-		return objList;
-	}
+    private List<T> readExcel(InputStream is) throws Exception {
+        Workbook workbook = WorkbookFactory.create(is);
+        int sheetCount = Math.min(workbook.getNumberOfSheets(), MAX_SHEET);
+        List<T> objList = new ArrayList<>();
+        sheetCount = (info.sheets == 0) ? sheetCount : info.sheets;
+        for (int i = 0; i < sheetCount; i++) {
+            Sheet sheet = workbook.getSheetAt(i);
+            extractSheet(sheet, objList);
+        }
+        return objList;
+    }
 
-	private List<T> readCsv(InputStream in) throws IOException {
-		List<T> objList = new ArrayList<>();
-		InputStreamReader isr = new InputStreamReader(in);
-		try (CSVReader cvsr = new CSVReader(isr, info.delimiter)) {
-			List<String[]> allRows = cvsr.readAll();
-			int start = info.hasHeader ? 1 : 0;
-			for (int i = start; i < allRows.size(); i++) {
-				T obj = info.converter.convert(allRows.get(i));
-				objList.add(obj);
-			}
-		}
-		return objList;
-	}
+    private List<T> readCsv(InputStream in) throws Exception {
+        List<T> objList = new ArrayList<>();
+        InputStreamReader isr = new InputStreamReader(in);
+        try (CSVReader cvsr = new CSVReader(isr, info.delimiter)) {
+            List<String[]> allRows = cvsr.readAll();
+            int start = info.hasHeader ? 1 : 0;
+            for (int i = start; i < allRows.size(); i++) {
+                T obj = info.converter.convert(allRows.get(i));
+                objList.add(obj);
+            }
+        }
+        return objList;
+    }
 
-	private void extractSheet(Sheet sheet, List<T> objList) {
-		Iterator<Row> rowIterator = sheet.iterator();
-		if (rowIterator.hasNext() && info.hasHeader) {
-			rowIterator.next();
-		}
-		while (rowIterator.hasNext()) {
-			T obj = extractObject(rowIterator);
-			objList.add(obj);
-		}
-	}
+    private void extractSheet(Sheet sheet, List<T> objList) {
+        Iterator<Row> rowIterator = sheet.iterator();
+        if (rowIterator.hasNext() && info.hasHeader) {
+            rowIterator.next();
+        }
+        while (rowIterator.hasNext()) {
+            T obj = extractObject(rowIterator);
+            objList.add(obj);
+        }
+    }
 
-	private T extractObject(Iterator<Row> rowIterator) {
-		Row row = rowIterator.next();
-		Iterator<Cell> cellIterator = row.cellIterator();
-		String[] rowVals = new String[row.getLastCellNum()];
-		while (cellIterator.hasNext()) {
-			Cell cell = cellIterator.next();
-			rowVals[cell.getColumnIndex()] = getValue(cell);
-		}
-		return info.converter.convert(rowVals);
-	}
+    private T extractObject(Iterator<Row> rowIterator) {
+        Row row = rowIterator.next();
+        Iterator<Cell> cellIterator = row.cellIterator();
+        Object[] rowVals = new Object[row.getLastCellNum()];
+        while (cellIterator.hasNext()) {
+            Cell cell = cellIterator.next();
+            rowVals[cell.getColumnIndex()] = getValue(cell);
+        }
+        return info.converter.convert(rowVals);
+    }
 
-	private boolean isExcel(InputStream is) throws IOException {
-		return POIXMLDocument.hasOOXMLHeader(is) /* .xlsx */
-				|| POIFSFileSystem.hasPOIFSHeader(is); /* .xls */
-	}
+    private boolean isExcel(InputStream is) throws Exception {
 
-	private String getValue(Cell cell) {
-		switch (cell.getCellType()) {
-		case Cell.CELL_TYPE_STRING:
-			return cell.getStringCellValue();
-		case Cell.CELL_TYPE_NUMERIC:
-			return String.valueOf(cell.getNumericCellValue());
-		}
-		return null;
-	}
+        return POIXMLDocument.hasOOXMLHeader(is) /* .xlsx */
+                || POIFSFileSystem.hasPOIFSHeader(is); /* .xls */
+    }
+
+    private Object getValue(Cell cell) {
+        switch (cell.getCellType()) {
+            case Cell.CELL_TYPE_STRING:
+                return cell.getStringCellValue();
+            case Cell.CELL_TYPE_NUMERIC:
+                return cell.getNumericCellValue();
+            case Cell.CELL_TYPE_BOOLEAN:
+                return cell.getBooleanCellValue();
+            case Cell.CELL_TYPE_ERROR:
+                return cell.getErrorCellValue();
+            case Cell.CELL_TYPE_FORMULA:
+                return cell.getCellFormula();
+            case Cell.CELL_TYPE_BLANK:
+                return null;
+        }
+        return null;
+    }
 
 }
--- src/test/java/co/elron/excelr/ExcelTest.java
+++ src/test/java/co/elron/excelr/ExcelTest.java
@@ -1,12 +1,13 @@
 package co.elron.excelr;
 
-import java.util.List;
-
-import org.junit.Assert;
-import org.junit.Test;
-
 import co.elron.utils.excelr.ExcelReader;
 import co.elron.utils.excelr.ExcelReader.RowConverter;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class ExcelTest {
 
@@ -20,40 +21,74 @@ public class ExcelTest {
 		}
 	}
 
-	static interface Run {
+	interface Run {
 		void run() throws Exception;
 	}
 
-	@Test
-	public void test() throws Exception {
-		RowConverter<Country> converter = (row) -> new Country(row[0], row[1]);
-		//
-		ExcelReader<Country> reader = ExcelReader.builder(Country.class).converter(converter).withHeader().csvDelimiter(';').sheets(1).build();
-		//
-		List<Country> list;
-		list = reader.read("src/test/resources/CountryCodes.xlsx");
-		checkFirst(list.get(0));
-		list = reader.read("src/test/resources/CountryCodes.xls");
-		checkFirst(list.get(0));
-		list = reader.read("src/test/resources/CountryCodes.csv");
-		checkFirst(list.get(0));
+	private ExcelReader<Country> reader;
+
+	@Before
+	public void setUp() {
+		RowConverter<Country> converter = (row) -> new Country((String) row[0], (String) row[1]);
+		reader = ExcelReader.builder(Country.class)
+				            .converter(converter)
+				            .withHeader()
+				            .csvDelimiter(';')
+				            .sheets(1)
+				            .build();
 	}
 
-	private void checkFirst(Country c) {
-		Assert.assertEquals(c.shortCode, "ad");
-		Assert.assertEquals(c.name, "Andorra");
+	@Test
+	public void shouldParseCorrectly_GivenXlsxFile() throws Exception {
+		List<Country> list;
+		list = reader.read("src/test/resources/CountryCodes.xlsx");
+		checkList(list);
+	}
+
+	@Test
+	public void shouldParseCorrectly_GivenXlsFile() throws Exception {
+		List<Country> list;
+		list = reader.read("src/test/resources/CountryCodes.xls");
+		checkList(list);
+	}
+
+	@Test
+	public void shouldParseCorrectly_GivenCsvFile() throws Exception {
+		List<Country> list;
+		list = reader.read("src/test/resources/CountryCodes.csv");
+		checkList(list);
+	}
+
+	@Test
+	public void shouldHandleNullValues_GivenANullCell() throws Exception {
+		List<Country> list;
+		list = reader.read("src/test/resources/CountryCodes.xls");
+		Country country = list.get(1);
+		assertEquals(null, country.name);
+		assertEquals("ae", country.shortCode);
+	}
+
+	private void checkList(List<Country> list) {
+		assertEquals(252, list.size());
+		assertEquals(list.get(0).shortCode, "ad");
+		assertEquals(list.get(0).name, "Andorra");
+		//
+		assertEquals(list.get(56).shortCode, "dj");
+		assertEquals(list.get(56).name, "Djibouti");
+		//
+		assertEquals(list.get(243).shortCode, "wf");
+		assertEquals(list.get(243).name, "Wallis and Futuna Islands");
 	}
 
 	@Test
 	public void benchmark() throws Exception {
-		RowConverter<Country> converter = (row) -> new Country(row[0], row[1]);
+		RowConverter<Country> converter = (row) -> new Country((String) row[0], (String) row[1]);
 		//
 		ExcelReader<Country> reader = ExcelReader.builder(Country.class).converter(converter).csvDelimiter(';').sheets(1).build();
 		//
 		delta(() -> reader.read("src/test/resources/CountryCodes.xlsx"));
 		delta(() -> reader.read("src/test/resources/CountryCodes.xls"));
 		delta(() -> reader.read("src/test/resources/CountryCodes.csv"));
-		//
 	}
 
 	public void delta(Run c) throws Exception {
